08bdb79c03568c82e69c0963b03d0659395e18fa
pwu97 <43555461+pwu97@users.noreply.github.com>
2022-05-24 22:30:28 -0400

    Test added branch.

--- C++/Sample.cpp
+++ C++/Sample.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     cout << "This is my first C++ program." << endl;
     cout << "This is mybranch code." << endl;
+    cout << "This is mybranch code 2." << endl;
     return 0;
 }
