83833a7bfd9d0439f8280af387ad7006c5f47eed
pwu97 <43555461+pwu97@users.noreply.github.com>
2022-08-06 21:22:55 -0700

    Add LC 1509.

--- C++/countBits.cpp
+++ C++/countBits.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 short countBits(unsigned int x) {
--- C++/minAmplitude.cpp
+++ C++/minAmplitude.cpp
@@ -1,16 +1,20 @@
 // Link: https://leetcode.com/discuss/interview-question/553399/
 // Link: https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/discuss/?currentPage=1&orderBy=hot&query=
-// Given an Array A, find the minimum amplitude you can get after changing up to 3 elements. Amplitude is the range of the array (basically difference between largest and smallest element).
+
+// You are given an integer array nums. In one move, you can choose one element of nums and change it by any value.
+// Return the minimum difference between the largest and smallest value of nums after performing at most three moves.
 
 // Example 1:
-// Input: [-1, 3, -1, 8, 5 4]
-// Output: 2
-// Explanation: we can change -1, -1, 8 to 3, 4 or 5
+// Input: nums = [5,3,2,4]
+// Output: 0
+// Explanation: Change the array [5,3,2,4] to [2,2,2,2].
+// The difference between the maximum and minimum is 2-2 = 0.
 
 // Example 2:
-// Input: [10, 10, 3, 4, 10]
-// Output: 0
-// Explanation: change 3 and 4 to 10
+// Input: nums = [1,5,0,10,14]
+// Output: 1
+// Explanation: Change the array [1,5,0,10,14] to [1,1,0,1,1]. 
+// The difference between the maximum and minimum is 1-0 = 1.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -25,12 +29,12 @@ int minDifference(vector<int>& nums) {
 int main() {
     vector<int> v1 = {-1, 3, -1, 8, 5, 4};
     vector<int> v2 = {10, 10, 3, 4, 10};
-    
     cout << minDifference(v1) << endl;
     cout << minDifference(v2) << endl;
+    
     return 0;
 }
 
 // 2
 // 0
-// ./a.out  0.00s user 0.00s system 42% cpu 0.009 total
+// ./a.out  0.00s user 0.00s system 1% cpu 0.271 total
